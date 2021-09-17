120f9c83db8766f7513dc1b79714f59e8b57bd07
adekang <201497709@qq.com>
2021-09-17 15:38:25 +0800

    add readme

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -33,7 +33,6 @@ const Index = () => {
     get(1);
   }, []);
   const get = pageNum => {
-    console.log('调用了');
     return new Promise(resolve => {
       setTimeout(() => {
         const newList = new Array(pageNumber)
@@ -42,26 +41,23 @@ const Index = () => {
         setList(pageNumber === 1 ? newList : list.concat(newList));
         setPageNumber(pageNumber += pageNum);
         page.current += 1;
-        // setHasMore(pageNum < 4);
         resolve('ok');
       }, 800);
     });
   };
 
   const refresh = () => {
-    console.log('进来了');
-    return get(2);
+    return get(1);
   };
 
   return (
-    <div className="Fcontainer">
+    <div className="FContainer">
       <div className="box">123</div>
       <PullRefresh
         refresh={refresh}
-        distanceToRefresh={100}
+        distanceToRefresh={56}
         headerHeight={56}
         stayTime={300}
-        duration={300}
       >
         {list.map(index => (
           <RowRender index={index} key={index}/>
